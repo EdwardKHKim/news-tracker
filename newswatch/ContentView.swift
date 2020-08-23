32f7eb1ffbd128e0942012ca05b7bbebc1f062a0
EdwardKHKim <kim.kyoohoon.edward@gmail.com>
2020-08-23 02:02:54 -0700

    Initial Commit

---
+++ newswatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newswatch
+//
+//  Created by Edward Kim on 2020-08-23.
+//  Copyright © 2020 Edward Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
